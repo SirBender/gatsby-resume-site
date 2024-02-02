37557486e70bae76c9e1460a7223a64be3ece7ac
SirBender <eric.echelle@outlook.com>
2024-02-02 21:44:28 +0000

    adding first components

---
+++ src/pages/resume.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const ResumePage = () => {
+    return (
+      <Layout pageTitle="Resume">
+        <p>This page will contain my Resume.</p>
+      </Layout>
+    )
+}
+
+export const Head = () => <title>Resume</title>
+
+export default ResumePage
